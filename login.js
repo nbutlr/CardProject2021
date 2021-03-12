15ea23f363c8f1d893f60cd230cd213c41932126
nbutlr <05257@farnborough.ac.uk>
2021-03-12 12:06:41 +0000

    End of Lesson 4 commit

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+// This function just takes the inputs from the username and password and dumps it straight back into the console
 function submit() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Create the cards array with all the necessary cards
+// Create the cards array with all the necessary cards //
 function initCards() {
     let colours = ["Red","Yellow","Black"];
     for (let i=0;i<3;i++) {
@@ -8,7 +8,7 @@ function initCards() {
     }
 }
   
-// Shuffles an array
+// Shuffles an array //
 function shuffle(array) {
     let currentIndex = array.length, temp, randomIndex;
     while (0 !== currentIndex) {
@@ -21,59 +21,56 @@ function shuffle(array) {
     return array;
 }
 
-// Calculates the winner between 2 cards using the rules from the brief
+// Calculates the winner between 2 cards using the rules from the brief //
 function calculateWinner(card1,card2) {
     if (card1.colour === "Red" && card2.colour === "Black") {
-        document.getElementById("p1").innerHTML += "<br>P1 wins as Red beats Black.";
+        document.getElementById("p1").innerHTML += "<br>Player 1 wins as "+"<span style='color:red;'>Red</span>"+" beats "+"<span style='color:black;'>Black</span>"+".";
         p1cards.push(card1,card2);
         cards.splice(0,2);
     } else if (card1.colour === "Black" && card2.colour === "Red") {
-        document.getElementById("p1").innerHTML += "<br>P2 wins as Red beats Black.";
+        document.getElementById("p1").innerHTML += "<br>Player 2 wins as "+"<span style='color:red;'>Red</span>"+" beats "+"<span style='color:black;'>Black</span>"+".";
         p2cards.push(card1,card2);
         cards.splice(0,2);
     } else if (card1.colour === "Yellow" && card2.colour === "Red") {
-        document.getElementById("p1").innerHTML += "<br>P1 wins as Yellow beats Red.";
+        document.getElementById("p1").innerHTML += "<br>Player 1 wins as "+"<span style='color:#ffdd00;'>Yellow</span>"+" beats "+"<span style='color:red;'>Red</span>"+".";
         p1cards.push(card1,card2);
         cards.splice(0,2);
     } else if (card1.colour === "Red" && card2.colour === "Yellow") {
-        document.getElementById("p1").innerHTML += "<br>P2 wins as Yellow beats Red.";
+        document.getElementById("p1").innerHTML += "<br>Player 2 wins as "+"<span style='color:#ffdd00;'>Yellow</span>"+" beats "+"<span style='color:red;'>Red</span>"+".";
         p2cards.push(card1,card2);
         cards.splice(0,2);
     } else if (card1.colour === "Black" && card2.colour === "Yellow") {
-        document.getElementById("p1").innerHTML += "<br>P1 wins as Black beats Yellow.";
+        document.getElementById("p1").innerHTML += "<br>Player 1 wins as "+"<span style='color:black;'>Black</span>"+ " beats "+"<span style='color:#ffdd00;'>Yellow</span>"+".";
         p1cards.push(card1,card2);
         cards.splice(0,2);
     } else if (card1.colour === "Yellow" && card2.colour === "Black") {
-        document.getElementById("p1").innerHTML += "<br>P2 wins as Black beats Yellow.";
+        document.getElementById("p1").innerHTML += "<br>Player 2 wins as "+"<span style='color:black;'>Black</span>"+" beats "+"<span style='color:#ffdd00;'>Yellow</span>"+".";
         p2cards.push(card1,card2);
         cards.splice(0,2);
     } else {
         // This last selection works regardless of colour, so it does not need separating //
         if (card1.value > card2.value) {
-            document.getElementById("p1").innerHTML += "<br>P1 wins as "+cards[0].value+" is greater than "+cards[1].value+".";
+            document.getElementById("p1").innerHTML += "<br>Player 1 wins as "+cards[0].value+" is greater than "+cards[1].value+".";
             p1cards.push(card1,card2);
             cards.splice(0,2);
         } else {
             // No chance of draw as each card is unique //
-            document.getElementById("p1").innerHTML += "<br>P2 wins as "+cards[1].value+" is greater than "+cards[0].value+".";
+            document.getElementById("p1").innerHTML += "<br>Player 2 wins as "+cards[1].value+" is greater than "+cards[0].value+".";
             p2cards.push(card1,card2);
             cards.splice(0,2);
         }
     }
 }
 
-// This function plays out 1 round and is run every time the button is clicked
+// This function plays out 1 round and is run every time the button is clicked //
 function round() {
     roundCounter++;
     document.getElementById("round").innerHTML = "Round "+roundCounter;
+    // Makes the yellow readable //
     let card0colour = cards[0].colour;
-    if (card0colour === "Yellow") {
-        card0colour = "#ffdd00";
-    }
+    if (card0colour === "Yellow") {card0colour = "#ffdd00";}
     let card1colour = cards[1].colour;
-    if (card1colour === "Yellow") {
-        card1colour = "#ffdd00";
-    }
+    if (card1colour === "Yellow") {card1colour = "#ffdd00";}
     document.getElementById("p1").innerHTML = "Player 1's card is "+"<span style='color:"+card0colour+";'>"+cards[0].colour+" "+cards[0].value+"</span>"+"<br>Player 2's card is "+"<span style='color:"+card1colour+"'>"+cards[1].colour+" "+cards[1].value+"</span>";
     calculateWinner(cards[0],cards[1]);
     document.getElementById("p2").innerHTML = "P1 has "+p1cards.length+" cards.<br>P2 has "+p2cards.length+" cards.<br>There are "+cards.length+" cards left in the deck.";
@@ -84,17 +81,29 @@ function round() {
     } 
 }
 
-// Outputs the overall winner of the game
+// Outputs the overall winner of the game //
 function overallWinner() {
     if (p1cards.length > p2cards.length) {
-        document.getElementById("p3").innerHTML = "Player 1 is the winner!";
+        document.getElementById("p3").innerHTML = "Player 1 is the winner!<br>Cards: ";
+        for (let i=0;i<p1cards.length;i++) {
+            let cardcolour = p1cards[i].colour;
+            if (cardcolour === "Yellow") {cardcolour = "#ffdd00";}
+            document.getElementById("p3").innerHTML += "<span style='color:"+cardcolour+";'>"+p1cards[i].colour+" "+p1cards[i].value+"</span>" + ", ";
+        }
     } else {
         // There cannot be a draw as cards are moved in groups of 2 //
         document.getElementById("p3").innerHTML = "Player 2 is the winner!";
+        for (let j = 0; j < p2cards.length; j++) {
+            let cardcolour = p2cards[j].colour;
+            if (cardcolour === "Yellow") {
+            cardcolour = "#ffdd00";
+            }
+            document.getElementById("p3").innerHTML += "<span style='color:" + cardcolour + ";'>" + p2cards[j].colour + " " + p2cards[j].value + "</span>" + ", ";
+        }
     }
 }
 
-// Card class
+// Card class //
 class Card {
     constructor(colour,value) {
         this.colour = colour;
@@ -102,7 +111,7 @@ class Card {
     }
 }
   
-// Main Program
+// Main Program - initialises lists and cards and stuff //
 cards=[];
 p1cards=[];
 p2cards=[];
